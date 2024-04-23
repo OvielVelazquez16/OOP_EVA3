8321e2ef2619fdb710a596999baf373d073e38e4
OvielVelazquez16 <158379953+OvielVelazquez16@users.noreply.github.com>
2024-04-23 17:58:21 -0600

    Add files via upload

---
+++ EVA3_1_EXCEPCIONES/src/main/java/com/mycompany/eva3_1_excepciones/EVA3_1_EXCEPCIONES.java
@@ -0,0 +1,21 @@
+
+
+package com.mycompany.eva3_1_excepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author oviel
+ */
+public class EVA3_1_EXCEPCIONES {
+
+    public static void main(String[] args) {
+        int num;
+        Scanner captu = new Scanner (System.in);
+        System.out.println("Introduce un valor entero:");
+        num = captu.nextInt();
+        System.out.println("El valor es " + num);
+        
+    }
+}
